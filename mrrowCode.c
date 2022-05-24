30597738b7593286f2f679a082903ec08936be9a
AriCastel <106104460+AriCastel@users.noreply.github.com>
2022-05-24 11:53:55 -0500

    Update mrrowCode.c

--- mrrowCode.c
+++ mrrowCode.c
@@ -4,11 +4,11 @@
 #include <DHT_U.h>
 
 //Pins
-    int pinPump = 13;
+    int pinPump = 12;
     int sensorWater = A0;
-    int pinLED = 12;
+    int pinLED = 13;
     int sensorPanel = A1;
-    int pinFan = 11;
+    int pinFan = 7;
     int sensorHeat = 10; 
 
 
@@ -18,42 +18,48 @@
 DHT dht(DHTPIN, DHTTYPE);
 
 //Times that each function will be executed for
-    int timePump = 10000;
-    int timeFan = 10000;
-    int timeLed = 10000;
+    int timePump = 5000;
+    int timeFan = 20000;
+    int timeLed = 20000;
+
+//Global times
+long TSLR=0;; //Time Since Last Reading
+long TSLP = 0;; //Time since last pump activation
 
 //Limits, thresholds that will trigger each action
     int minWater = 700;
-    float minLight = 2.0;
+    float minLight = 400.0;
     float maxHeat = 20.0;
-    float maxWet = 30.0;
+    float maxWet = 70.0;
 
 //Functions 
     void waterControl(){ //Code for the Water Pump
-        Serial.print("reading Water Levels");
+        Serial.print("reading Water Levels: ");
         int wetness = analogRead(sensorWater);
         Serial.print(wetness);
   
-        if(wetness < minWater){
-            Serial.println("Water Pump engaged");
-            digitalWrite(pinPump, HIGH); 
-            delay(timePump);
-            digitalWrite(pinPump, LOW);
+        if(wetness < minWater && (millis()- TSLP)>60000)
+        {
+        Serial.println("-Water Pump engaged-");
+        digitalWrite(pinPump, HIGH); 
+        delay(timePump);
+        digitalWrite(pinPump, LOW);
+        TSLP = millis(); 
         }
     }
 
     void temperatureControl(){ //Code for the Fan 
-        Serial.print("reading Atmospheric Conditions");
+        Serial.print("reading Atmospheric Conditions: ");
         float humidity = dht.readHumidity(); //Reading Humidity
         float temperature = dht.readTemperature(); //Reading temperature
         Serial.print("Humidity ");
         Serial.print(humidity);
-        Serial.print("pct");
-        Serial.print("Temperature: ");
+        Serial.print(" pct/");
+        Serial.print(" Temperature: ");
         Serial.print(temperature);
-        Serial.print(" *C ");
-
-        if (temperature > maxHeat || humidity > 60.0){
+        Serial.print(" *C /");
+        
+        if (temperature >= maxHeat || humidity >= maxWet){
             Serial.println("Fan Engaged");
             digitalWrite(pinFan, HIGH);
             delay(timeFan);
@@ -64,15 +70,15 @@ DHT dht(DHTPIN, DHTTYPE);
      }
 
     void lightControl(){ //Code for the LED array
-        Serial.print("Reading Radiation Levels");
+        Serial.print("Reading Radiation Levels ");
         int light = analogRead(sensorPanel);
         Serial.println(light);
 
         if (light < minLight ){
-            Serial.println("LEDs engaged");
-            digitalWrite(pinLED, HIGH); 
-            delay(timeLed);
-            digitalWrite(pinLED, LOW);         
+        Serial.println("LEDs engaged");
+        digitalWrite(pinLED, HIGH); 
+        delay(timeLed);
+        digitalWrite(pinLED, LOW);         
         }
 
 
@@ -134,31 +140,36 @@ void setup() {
 
     Serial.begin(9600);
     Serial.println("Initializing...");
-    
-    dht.begin();
+        
 
-    // Pin Modes 
-        pinMode(pinPump, OUTPUT);
-        pinMode(pinLED, OUTPUT);
-        pinMode(pinFan, OUTPUT);
-        pinMode(sensorHeat, INPUT);
-        pinMode(LED_BUILTIN, OUTPUT);
+        dht.begin();
 
-    testFan();
-    testLED();
-    testPump();
-    Serial.println("Set Up done");       
+        // Pin Modes 
+            pinMode(pinPump, OUTPUT);
+            pinMode(pinLED, OUTPUT);
+            pinMode(pinFan, OUTPUT);
+            pinMode(sensorHeat, INPUT);
+            pinMode(LED_BUILTIN, OUTPUT);
+
+        testFan();
+        delay(1000);
+        testLED();
+        delay(1000);
+        testPump();
+        delay(1000);
+        Serial.println("Set Up done");       
 } 
 //Loop
-long TSLR=0;; //Time Since Last Reading
+
 void loop() {
-    if(millis()- TSLR>2000){ //Performs the actions
-        temperatureControl();
-        waterControl();
-        lightControl();
-        Serial.println("Readings done");
+if(millis()- TSLR>5000){ //Performs the actions
+    temperatureControl();
+    waterControl();
+    lightControl();
+    Serial.println("Readings done");
+    TSLR =millis(); //Time Since Last reading Updated 
 }
 
-    TSLR =millis(); //Time Since Last reading Updated 
+    
 
 }
